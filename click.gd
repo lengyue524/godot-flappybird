f3665fbad7a900bfd1797d896e90bbfecbccba48
李航 <lengyue524@gmail.com>
2015-04-03 18:49:16 +0800

    修复移动设备点击。

--- click.gd
+++ click.gd
@@ -7,7 +7,11 @@ extends Button
 
 func _ready():
 	# Initalization here
+	set_process_input(true)
 	pass
 
-func _pressed():
-	get_node("/root/global").goto_scene("res://main.scn")
+#func _pressed():
+#	get_node("/root/global").goto_scene("res://main.scn")
+func _input_event(event):
+	if ((event.type == InputEvent.SCREEN_TOUCH or event.type == InputEvent.MOUSE_BUTTON)and event.pressed):
+		get_node("/root/global").goto_scene("res://main.scn")
